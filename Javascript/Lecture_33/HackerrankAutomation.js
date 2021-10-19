cde0a94043c186b44c8cb8cbf3f0586ecdd2e826
hema8Codes <developer8hema@gmail.com>
2021-10-19 20:41:53 +0530

    Dev Lecture 33

---
+++ Javascript/Lecture_33/HackerrankAutomation.js
@@ -0,0 +1,198 @@
+// node HackerrankAutomation.js --url=https://www.hackerrank.com --config=config.json
+
+// npm init --yes
+// npm install minimist
+// npm install puppeteer-core
+
+let minimist = require("minimist");
+let fs = require("fs");
+let puppeteer = require("puppeteer");
+
+let args = minimist(process.argv);
+// console.log(args.url);
+// console.log(args.config);
+let configJSON = fs.readFileSync(args.config, "utf-8"); // utf-8 we are reading this string in the form of
+let configJSO = JSON.parse(configJSON);
+
+// console.log(configJSO.userid);
+// console.log(configJSO.password);
+// console.log(configJSO.moderator);
+
+//By Promise method
+
+//   let browserKaPromise = puppeteer.launch({headless: false});
+//   browserKaPromise.then(function(browser){
+//   let pagesKaPromise = browser.pages();
+//     pagesKaPromise.then(function(pages){
+//         let responseKaPromise = pages[0].goto(args.url);
+//         responseKaPromise.then(function(response){
+//             let browserCloseKaPromise = browser.close();
+//             browserCloseKaPromise.then(function(){
+//                 console.log("browser closed");
+//             })
+//         })
+//     })
+//   })
+
+// IIFE -> Immediately invoked function execution
+// (function(){
+
+// })();
+
+// we can write await only in that function that contains async in front of it
+
+//By Async Await Method
+
+// (async () => {
+//   let browser = await puppeteer.launch({headless: false});
+//   let page = await browser.newPage();
+//   await page.goto(args.url);
+//   await page.screenshot({ path: 'example.png' });
+
+//   await browser.close();
+// })();
+
+async function run(){
+    // start the browser
+    let browser = await puppeteer.launch({
+        headless: false,
+        args: [
+            '--start-maximized'
+        ],
+        defaultViewport: null
+    });
+
+    // get the tab (there is only one tab)
+    let pages = await browser.pages();
+    let page = pages[0];
+    
+    // open the url
+    await page.goto(args.url);
+
+    // wait and then click on login on page1
+    await page.waitForSelector("a[data-event-action='Login']");
+    await page.click("a[data-event-action='Login']");
+    
+    // wait and then click on login on page2
+    await page.waitForSelector("a[href='https://www.hackerrank.com/login']");
+    await page.click("a[href='https://www.hackerrank.com/login']");
+    
+    // type userid
+    await page.waitForSelector("input[name='username']");
+    await page.type("input[name='username']", configJSO.userid, {delay: 28});
+     
+    // type password
+    await page.waitForSelector("input[name='password']");
+    await page.type("input[name='password']", configJSO.password, {delay: 28});
+    
+    // press click on page3 
+    await page.waitForSelector("button[data-analytics='LoginPassword']");
+    await page.click("button[data-analytics='LoginPassword']");
+    
+    // click on compete
+    await page.waitForSelector("a[data-analytics='NavBarContests']");
+    await page.click("a[data-analytics='NavBarContests']");
+    
+    // click on manage contest
+    await page.waitForSelector("a[href='/administration/contests/']");
+    await page.click("a[href='/administration/contests/']");
+
+    // find number of pages
+    // await page.waitForSelector("a[data-attr1='Last']");
+    // let numPages = await page.$eval("a[data-attr1='Last']", function(atag){ // $eval() - > gives specific
+    //     let totpages = parseInt(atag.getAttribute("data-page"));
+    //     return totpages;
+    // })
+
+    // console.log(numPages);
+
+    for(let i = 1; i <= 1; i++){
+      await handleAllContestsOfAPage(page, browser);
+       
+    //   if(i != numPages){  
+        await page.waitForSelector("a[data-attr1='Right']");
+        await page.click("a[data-attr1='Right']");
+    //   }  
+
+    }
+
+}
+
+async function handleAllContestsOfAPage(page, browser){
+    // find all urls of same page
+    await page.waitForSelector("a.backbone.block-center");
+    let curls = await page.$$eval("a.backbone.block-center", function(atags){  //  $$eval() - > gives array
+         let urls = [];
+
+         for(let i = 0; i < atags.length; i++){
+             let url = atags[i].getAttribute("href");
+             urls.push(url);
+         }
+
+         return urls;
+    });
+    
+
+    for(let i = 0; i < curls.length; i++){
+        let curl = curls[i];
+
+        let ctab = await browser.newPage();
+        await SaveModeratorInContest(ctab, args.url + curl, configJSO.moderator)
+        await ctab.close();
+        await page.waitFor(3000);
+
+    }
+}
+
+async function SaveModeratorInContest(ctab, fullCurl, moderator){
+    await ctab.bringToFront();
+    await ctab.goto(fullCurl);
+    await ctab.waitFor(3000);
+
+    // click on moderators tab
+    await ctab.waitForSelector("li[data-tab='moderators']");
+    await ctab.click("li[data-tab='moderators']");
+
+    // type in moderator
+    await ctab.waitForSelector("input#moderator");
+    await ctab.type("input#moderator", moderator, {delay: 50});
+
+    // press enter
+    await ctab.keyboard.press("Enter");
+}
+
+run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+    // // click on first contest
+    // await page.waitForSelector("p.mmT");
+    // await page.click("p.mmT");
+    
+    // await page.waitFor(3000);
+    
+    // // click on moderators tab
+    // await page.waitForSelector("li[data-tab='moderators']");
+    // await page.click("li[data-tab='moderators']");
+
+    // // type in moderator
+    // await page.waitForSelector("input#moderator");
+    // await page.type("input#moderator", configJSO.moderator, {delay: 50});
+
+    // await page.keyboard.press("Enter");
